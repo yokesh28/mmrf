fa20df7ea76e28f056c1bdd244041a31b8c05e8b
lokesh <yokesh.ganesan@gmail.com>
2013-03-11 16:37:24 +0530

    Signed-off-by: lokesh <yokesh.ganesan@gmail.com>

--- mail.php
+++ mail.php
@@ -15,7 +15,79 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
-$message = '<html><body>';
+$message = '<table>
+<tr>
+<td>
+<img src="images/mmrf_logo.png" width="122" height="80" />
+</td>
+<td>MMRF Reality And Infrastructure Pvt Ltd... </td>
+</tr>
+<table border="1" >
+
+<tr>
+<td>
+Name:
+</td>
+<td>'.$_REQUEST['name'].'</td>
+</tr>
+
+<tr>
+<td>Address:</td>
+<td>Coimbatore</td>
+</tr>
+
+<tr>
+<td>Address:</td>
+<td>Coimbatore</td>
+</tr>
+
+<tr>
+<td>Email:</td>
+<td>annamalai.cs@gmail.com</td>
+</tr>
+
+<tr>
+<td>MobileNo:</td>
+<td>7401062094</td>
+</tr>
+
+<tr>
+<td>Location:</td>
+<td>Cbe</td>
+</tr>
+
+<tr>
+<td>BulidArea:</td>
+<td>10,000 to 50,000</td>
+</tr>
+
+<tr>
+<td>City:</td>
+<td>Coimbatore</td>
+</tr>
+
+<tr>
+<td>Pincode:</td>
+<td>6420094</td>
+</tr>
+
+<tr>
+<td>Phone No:</td>
+<td>0422-245643</td>
+</tr>
+
+<tr>
+<td>Budget:</td>
+<td>1,00,00000 to 3,00,00000</td>
+</tr>
+
+<tr>
+<td>Post Enquiry:</td>
+<td>I will purchase </td>
+</tr>
+</table>
+
+</table>';
 
 mail($to, $subject, $message, $headers);
 }
