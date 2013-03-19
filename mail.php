c879465c23bece44f10eb014197ac89001c9d37b
SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>
2013-03-19 18:48:20 +0530

    Signed-off-by: SHILLMEDIA <SHILLMEDIA@SHILLMEDIA-PC>

--- mail.php
+++ mail.php
@@ -1,92 +1,103 @@
 
 <?php
-if (isset($_REQUEST['email']))
 
-{
-	//send email
-	$to = 'info@mmrf.in,annamalai.cs@gmail.com';
 
-$subject = 'Website Change Reqest';
+ini_set('error_reporting', E_ALL);
+
+$to = 'annamalai.cs@gmail.com';
+
+$subject = 'Received mail information';
 
 $headers = "From: " . strip_tags($_POST['email']) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
-$headers .= "CC: susan@example.com\r\n";
+
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-
-$message = '<table>
-<tr>
-<td>
-<img src="images/mmrf_logo.png" width="122" height="80" />
+$message = '<html><body>';
+$message .='<table style="border-color: #666;">';
+$message .="<tr style='background: #eee;'><td><img src= 'http://mmrfrealty.com/images/mmrf_logo.png' width='122' height='80' />
 </td>
-<td>MMRF Reality And Infrastructure Pvt Ltd... </td>
-</tr>
-<table border="1" >
-
-<tr>
-<td>
-Name:
-</td>
-<td>'.$_REQUEST['name'].'</td>
-</tr>
-
-<tr>
-<td>Address:</td>
-<td>'.$_REQUEST['address'].'</td>
-</tr>
 
 
+<td><strong>MMRF Reality And Infrastructure Pvt Ltd... </strong></td>
+</tr>";
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_REQUEST['name']) . "</td></tr>";
+$message .= "<tr><td><strong>Address:</strong> </td><td>" . strip_tags($_REQUEST['address']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_REQUEST['email']) . "</td></tr>";
+$message .= "<tr><td><strong>Mobile:</strong> </td><td>" . strip_tags($_REQUEST['mobile']) . "</td></tr>";
 
-<tr>
-<td>Email:</td>
-<td>'.$_REQUEST['email'].'</td>
-</tr>
+$message .= "<tr><td><strong>Location:</strong> </td><td>" . strip_tags($_REQUEST['location']) . "</td></tr>";
+$message .= "<tr><td><strong>Bulidarea:</strong> </td><td>" . strip_tags($_REQUEST['build_area']) . "</td></tr>";
+$message .= "<tr><td><strong>City:</strong> </td><td>" . strip_tags($_REQUEST['city']) . "</td></tr>";
+$message .= "<tr><td><strong>Pincode:</strong> </td><td>" . strip_tags($_REQUEST['pincode']) . "</td></tr>";
 
-<tr>
-<td>MobileNo:</td>
-<td>'.$_REQUEST['mobile'].'</td>
-</tr>
+$message .= "<tr><td><strong>Phone:</strong> </td><td>" . strip_tags($_REQUEST['phone']) . "</td></tr>";
+$message .= "<tr><td><strong>Budget:</strong> </td><td>" . strip_tags($_REQUEST['budget']) . "</td></tr>";
+$message .= "<tr><td><strong>PostEnquiry:</strong> </td><td>" . strip_tags($_REQUEST['enquiry']) . "</td></tr>";
+$message .="</table>";
 
-<tr>
-<td>Location:</td>
-<td>'.$_REQUEST['location'].'</td>
-</tr>
+$message .= "</table>";
+$message .= "</body></html>";
 
-<tr>
-<td>BulidArea:</td>
-<td>'.$_REQUEST['bulid_area'].'</td>
-</tr>
 
-<tr>
-<td>City:</td>
-<td>'.$_REQUEST['city'].'</td>
-</tr>
 
-<tr>
-<td>Pincode:</td>
-<td>'.$_REQUEST['pincode'].'</td>
-</tr>
 
-<tr>
-<td>Phone No:</td>
-<td>'.$_REQUEST['phone'].'</td>
-</tr>
 
-<tr>
-<td>Budget:</td>
-<td>'.$_REQUEST['budget'].'</td>
-</tr>
 
-<tr>
-<td>Post Enquiry:</td>
-<td>'.$_REQUEST['enquiry'].' </td>
-</tr>
-</table>
 
-</table>';
 
-mail($to, $subject, $message, $headers);
+// Enter your email address
+
+$send_contact=mail($to,$subject,$message,$headers);
+
+// Check, if message sent to your email
+// display message "We've recived your information"
+if($send_contact){
+	echo "We've recived your contact information";
+}
+else {
+	echo "ERROR";
 }
 
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
